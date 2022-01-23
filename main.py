644d204b654efe40d5484fa7115bc69671fb5956
DevHub <56557924+Devhub56@users.noreply.github.com>
2022-01-23 10:38:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,4 +36,35 @@ class MainApp(App):
         equals_button.bind(on_press=self.on_solution)
         main_layout.add_widget(equals_button)
 
-        return main_layout
+        return main_layout 
+    
+def on_button_press(self, instance):
+    current = self.solution.text
+    button_text = instance.text
+
+    if button_text == "C":
+        # Clear the solution widget
+        self.solution.text = ""
+    else:
+        if current and (
+            self.last_was_operator and button_text in self.operators):
+            # Don't add two operators right after each other
+            return
+        elif current == "" and button_text in self.operators:
+            # First character cannot be an operator
+            return
+        else:
+            new_text = current + button_text
+            self.solution.text = new_text
+    self.last_button = button_text
+    self.last_was_operator = self.last_button in self.operators
+    
+    def on_solution(self, instance):
+    text = self.solution.text
+    if text:
+        solution = str(eval(self.solution.text))
+        self.solution.text = solution
+    
+    if __name__ == "__main__":
+    app = MainApp()
+    app.run()
